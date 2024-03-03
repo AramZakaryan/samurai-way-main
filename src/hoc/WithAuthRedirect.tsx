01bf3477e9c8e25baeccc60736184c0a980fb4c5
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-03-03 15:05:50 +0400

    Final Result of Lesson 69 (with HOC types)

--- src/hoc/WithAuthRedirect.tsx
+++ src/hoc/WithAuthRedirect.tsx
@@ -4,33 +4,77 @@ import {connect} from "react-redux";
 import {stateReduxType} from "../redux/storeRedux";
 
 
+type mapStateToPropsType = {
+    isAuth: boolean
+}
+
+type lala = mapStateToPropsType
+
+const mapStateToProps = (state: stateReduxType): mapStateToPropsType => {
+    return {
+        isAuth: state.authPartData.authData.isAuth
+    }
+}
+
+
+
+
 /** (Aram) Wrapper HOC which redirects to login page if the user is not authorised
  */
-export const withAuthRedirect = (InitialComponent: any) => {
+export function withAuthRedirect<T>(InitialComponent: React.ComponentType<T>) {
 
-    class ClassComponent extends React.Component<any> {
-        render() {
-            if (!this.props.isAuth) {
-                return <Redirect to={"/login"}/>
-            }
-            return <InitialComponent {...this.props}/>;
+    function TransformerComponent(props: mapStateToPropsType) {
+        const {isAuth, ...restProps} = props
+        // console.log(props)
+        if (!isAuth) {
+            return <Redirect to={"/login"}/>
         }
+        return <InitialComponent {...restProps as T} />;
     }
 
-    const mapStateToProps = (state: stateReduxType) => {
-        return {
-            isAuth: state.authPartData.authData.isAuth
-        }
+    const ConnectedComponent = connect(mapStateToProps)(TransformerComponent)
 
-    }
-
-    const ConnectComponent = connect(mapStateToProps)(ClassComponent)
-
-    return ConnectComponent
+    return ConnectedComponent
 
     // Structure
     // InitialComponent >>>
-    //    ClassComponent >>>
-    //       ConnectComponent
+    //    TransformerComponent >>>
+    //       ConnectedComponent
 
 }
+
+
+// // version with class component inside
+
+// export const withAuthRedirect = (InitialComponent: any) => {
+//
+//     class ClassComponent extends React.Component<any> {
+//         render() {
+//             if (!this.props.isAuth) {
+//                 return <Redirect to={"/login"}/>
+//             }
+//             return <InitialComponent {...this.props}/>;
+//         }
+//     }
+//
+//     const mapStateToProps = (state: stateReduxType) => {
+//         return {
+//             isAuth: state.authPartData.authData.isAuth
+//         }
+//
+//     }
+//
+//     const ConnectComponent = connect(mapStateToProps)(ClassComponent)
+//
+//     return ConnectComponent
+//
+//     // Structure
+//     // InitialComponent >>>
+//     //    ClassComponent >>>
+//     //       ConnectComponent
+//
+// }
+
+
+
+
