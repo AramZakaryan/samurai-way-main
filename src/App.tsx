eb5cc11325f466feff03b14835cad9c363c1c5a4
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-03-24 00:03:37 +0400

    interim commit of lesson 76

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import "./App.css"
 
+
 import { Navbar } from "components/Navbat/Navbar"
 import { BrowserRouter, Route } from "react-router-dom"
 import { News } from "components/News/News"
